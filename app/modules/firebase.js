b4b16782984b61d8d4a2b2866b87b19a27c3cb4a
Minku Lee <premist@me.com>
2015-12-20 13:51:05 +0900

    Use Fireproof for async Firebase access

--- app/modules/firebase.js
+++ app/modules/firebase.js
@@ -3,4 +3,7 @@ const Promise = require("bluebird");
 const Firebase = require("firebase");
 const firebase = new Firebase(process.env.FIREBASE_URL);
 
-module.exports = Promise.promisifyAll(firebase);
+const Fireproof = require("fireproof");
+const fireproof = new Fireproof(firebase);
+
+module.exports = fireproof;
