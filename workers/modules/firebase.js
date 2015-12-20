368b080f3f2f28593c29bed1b8a4cc0aa8e1168f
Minku Lee <premist@me.com>
2015-12-20 19:20:42 +0900

    Copy firebase module from web server

---
+++ workers/modules/firebase.js
@@ -0,0 +1,9 @@
+const Promise = require("bluebird");
+
+const Firebase = require("firebase");
+const firebase = new Firebase(process.env.FIREBASE_URL);
+
+const Fireproof = require("fireproof");
+const fireproof = new Fireproof(firebase);
+
+module.exports = fireproof;
