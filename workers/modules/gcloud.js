80c32b679382e9354eb86abdcc867b839a46e8c4
Minku Lee <premist@me.com>
2015-12-20 18:28:59 +0900

    Implement gcloud.js / gcs.js for uploading

---
+++ workers/modules/gcloud.js
@@ -0,0 +1,9 @@
+const config = {
+  projectId: process.env.GCLOUD_PROJECT_ID,
+  credentials: {
+    client_email: process.env.GCLOUD_CLIENT_EMAIL,
+    private_key: process.env.GCLOUD_PRIVATE_KEY
+  }
+}
+
+module.exports = require("gcloud")(config);
---
+++ workers/modules/gcs.js
@@ -0,0 +1,33 @@
+const gcloud = require("./gcloud");
+const gcs = gcloud.storage();
+
+exports.uploadFile = function(buffer, bucket_name, path) {
+  var promise = new Promise(function(resolve, reject) {
+    var bucket = gcs.bucket(bucket_name);
+    var file = bucket.file(path);
+
+    var writeStream = file.createWriteStream();
+
+    writeStream.on("error", function(e) {
+      console.log("Upload failed!");
+      reject(e);
+    });
+
+    writeStream.on("finish", function() {
+      file.makePublic(function(e, resp) {
+        if(e) {
+          console.log("Upload failed!");
+          reject(e);
+          return;
+        }
+        console.log("Upload success!");
+        resolve(file);
+      });
+    });
+
+    writeStream.write(buffer);
+    writeStream.end();
+  });
+
+  return promise;
+}
