f39886d01cc39bb963968b1ecfc308f60e99fbd9
Minku Lee <premist@me.com>
2015-12-20 20:20:42 +0900

    Export gcs client on gcs module

--- workers/modules/gcs.js
+++ workers/modules/gcs.js
@@ -1,6 +1,8 @@
 const gcloud = require("./gcloud");
 const gcs = gcloud.storage();
 
+exports.client = gcs;
+
 exports.uploadFile = function(buffer, bucket_name, path) {
   var promise = new Promise(function(resolve, reject) {
     var bucket = gcs.bucket(bucket_name);
