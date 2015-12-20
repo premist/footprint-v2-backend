893a370452489b0e28494ac36535268299545c1d
Minku Lee <premist@me.com>
2015-12-20 20:20:57 +0900

    Implement swarm.checkin

--- workers/modules/swarm.js
+++ workers/modules/swarm.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const swarmAPIVersion = "20151220";
 
 var swarmInstance = axios.create({
   baseURL: "https://api.foursquare.com/v2",
@@ -8,7 +9,13 @@ var swarmInstance = axios.create({
 exports.swarmInstance = swarmInstance;
 
 exports.getVenue = function(venueId) {
-  var qs = `client_id=${process.env.FOURSQUARE_CLIENT_ID},client_secret=${process.env.FOURSQUARE_CLIENT_SECRET}`
+  var qs = `client_id=${process.env.FOURSQUARE_CLIENT_ID}&client_secret=${process.env.FOURSQUARE_CLIENT_SECRET}&v=${swarmAPIVersion}`
 
-  return swarmInstance.get(`/v2/venues/${venueId}?${qs}`);
+  return swarmInstance.get(`/venues/${venueId}?${qs}`);
+}
+
+exports.checkin = function(venueId, latitude, longitude, accessToken) {
+  var ll = `${latitude},${longitude}`
+  var qs = `oauth_token=${accessToken}&v=${swarmAPIVersion}&venueId=${venueId}&ll=${ll}`
+  return swarmInstance.post(`/checkins/add?${qs}`);
 }
