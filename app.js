554bcca4eff94de7dbe59f6c60198bd141a1224d
Minku Lee <premist@me.com>
2015-12-20 12:29:13 +0900

    Revise app loading

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
 require("dotenv").config({ silent: true });
 
+const fs = require("fs");
+const path = require("path");
+
 const koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 
 const app = koa();
 
+const router = require("./app/router");
 const grant = require("./app/modules/grant");
 const session = require("./app/modules/session");
-const router = require("./app/router");
+const db = require("./app/modules/db");
 
+// Load models
+var modelsPath = path.join(__dirname, "app/models");
+fs.readdirSync(modelsPath)
+  .forEach(file => require(path.join(modelsPath, file)));
+
+// Set session key
 app.keys = [process.env.SESSION_KEY];
 
+// Configure koa application
 app.use(bodyParser())
    .use(session)
    .use(grant)
    .use(router.routes())
    .use(router.allowedMethods());
 
-app.listen(process.env.PORT || 3000);
+db.on("error", function (e) {
+  console.log("Error while connecting to the database.");
+  console.log(e);
+}).once("open", function() {
+  console.log("Established MongoDB connection.");
+  startServer();
+});
+
+var startServer = function() {
+  var port = process.env.PORT || 3000;
+  app.listen(port);
+  console.log("next-one-api is now running on port " + port);
+}
